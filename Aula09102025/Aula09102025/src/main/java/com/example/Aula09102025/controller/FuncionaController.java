dc401b4b2c8137148a93df54c9606de3965f6f8b
Adalberto <adalberto.fernanndes@ba.estudante.senai.br>
2025-10-10 07:28:47 -0300

    feat: Atualização de alguns package controller / service / repository

--- Aula09102025/Aula09102025/src/main/java/com/example/Aula09102025/controller/FuncionaController.java
+++ Aula09102025/Aula09102025/src/main/java/com/example/Aula09102025/controller/FuncionaController.java
@@ -3,24 +3,41 @@ package com.example.Aula09102025.controller;
 import com.example.Aula09102025.model.FuncionarioModel;
 import com.example.Aula09102025.service.FuncionaroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/funcionarios")
 public class FuncionaController {
     @Autowired
-
     private FuncionaroService funcionaroService;
 
     @GetMapping
-    public List<FuncionarioModel>listar(){
-        return  funcionaroService.listarTodos();
+    public List<FuncionarioModel>ListaTodos(){
+        return funcionaroService.listarTodos();
     }
 
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> Salvar(@RequestBody  FuncionarioModel funcionarioModel){
+        funcionaroService.salvar(funcionarioModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("Message","Cadastrado com sucesso!"));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, Object>>atualizar(Long id, @RequestBody FuncionarioModel funcionarioModel){
+        funcionaroService.atualizar(id, funcionarioModel);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("Message", "Atualizado com sucesso"));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, Object>>deletefuncionario(@PathVariable Long id){
+        funcionaroService.excluir(id);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("Message", "Funcionario deletetado!"));
+    }
 
 }
--- Aula09102025/Aula09102025/src/main/java/com/example/Aula09102025/service/FuncionaroService.java
+++ Aula09102025/Aula09102025/src/main/java/com/example/Aula09102025/service/FuncionaroService.java
@@ -1,4 +1,49 @@
 package com.example.Aula09102025.service;
 
+import com.example.Aula09102025.model.FuncionarioModel;
+import com.example.Aula09102025.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FuncionaroService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    public List<FuncionarioModel>listarTodos(){
+        return funcionarioRepository.findAll();
+    }
+
+    public FuncionarioModel salvar(FuncionarioModel funcionarioModel){
+        if(funcionarioRepository.findByEmail(funcionarioModel.getEmail()).isPresent()){
+            throw new IllegalArgumentException("Funcionario já cadastrado");
+        }
+
+        return funcionarioRepository.save(funcionarioModel);
+    }
+
+    public FuncionarioModel atualizar(Long id, FuncionarioModel funcionarioModel){
+        if(!funcionarioRepository.existsById(id)){
+            throw new IllegalArgumentException("Funcionario não cadastrado");
+        }
+
+        funcionarioModel.setId(id);
+        return funcionarioRepository.save(funcionarioModel);
+    }
+
+    public void excluir(Long id){
+        if(!funcionarioRepository.existsById(id)){
+            throw new RuntimeException("Funcionario não encontrado");
+        }
+
+        funcionarioRepository.deleteById(id);
+
+    }
+
+    public void deletecliente(Long id){
+
+    }
 }
